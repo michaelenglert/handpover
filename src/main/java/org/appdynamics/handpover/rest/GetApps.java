cc68dd7525f6eb54763b771c864e83812893f515
michaelenglert <michi.eng@gmail.com>
2016-11-01 13:58:09 +0100

    Separated Folder Creation and included Progress definition

--- src/main/java/org/appdynamics/handpover/rest/GetApps.java
+++ src/main/java/org/appdynamics/handpover/rest/GetApps.java
@@ -4,9 +4,9 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.jersey.api.client.ClientResponse;
 import org.appdynamics.handpover.config.Globals;
+import org.appdynamics.handpover.export.Folder;
 import org.appdynamics.handpover.json.Apps;
 
-import java.io.File;
 import java.util.List;
 
 /**
@@ -30,21 +30,19 @@ public class GetApps implements Runnable {
         response = Base.getClientResponse(Globals.URL + Globals.CONTROLLER_ROOT + Globals.API_APP_LIST + Globals.API_OUTPUT);
         String output = response.getEntity(String.class);
         appList = objectMapper.readValue(output, mapType);
-        File appSettingsFolder = new File (Globals.APP_SETTINGS_FOLDER);
 
-        if (!appSettingsFolder.exists() && !output.isEmpty()) {
-            Boolean result = appSettingsFolder.mkdir();
-            if (!result) {
-                throw new RuntimeException(Globals.ERROR_FOLDER);
+        if (!output.isEmpty()) {
+            Folder.createFolder(Globals.APP_SETTINGS_FOLDER);
+            for (Apps app : appList) {
+                GetAppSettings.doGetAppSettings(app);
+                GetBts.doGetBusinessTransactions(app);
+                GetTiers.doGetTiers(app);
+                GetNodes.doGetNodes(app);
             }
+            Globals.PROGRESS = Globals.PROGRESS + 40;
         }
 
-        for (Apps app : appList) {
-            GetAppSettings.doGetAppSettings(app);
-            GetBts.doGetBusinessTransactions(app);
-            GetTiers.doGetTiers(app);
-            GetNodes.doGetNodes(app);
-        }
+
     }
 
 }
