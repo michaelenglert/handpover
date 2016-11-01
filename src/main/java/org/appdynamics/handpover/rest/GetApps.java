f58fd8eaf4844c9f723c3d6eac709b0b4123295f
michaelenglert <michi.eng@gmail.com>
2016-11-01 09:59:27 +0100

    Implements Runnable

--- src/main/java/org/appdynamics/handpover/rest/GetApps.java
+++ src/main/java/org/appdynamics/handpover/rest/GetApps.java
@@ -6,14 +6,22 @@ import com.sun.jersey.api.client.ClientResponse;
 import org.appdynamics.handpover.config.Globals;
 import org.appdynamics.handpover.json.Apps;
 
+import java.io.File;
 import java.util.List;
 
 /**
  * Created by michi on 28.08.16.
  */
-public class GetApps {
+public class GetApps implements Runnable {
+    public void run() {
+        try {
+            doGetApps();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-    public static void doGetApps() throws Exception {
+    private static void doGetApps() throws Exception {
         ClientResponse response;
         List<Apps> appList;
         ObjectMapper objectMapper = new ObjectMapper();
@@ -22,6 +30,14 @@ public class GetApps {
         response = Base.getClientResponse(Globals.URL + Globals.CONTROLLER_ROOT + Globals.API_APP_LIST + Globals.API_OUTPUT);
         String output = response.getEntity(String.class);
         appList = objectMapper.readValue(output, mapType);
+        File appSettingsFolder = new File (Globals.APP_SETTINGS_FOLDER);
+
+        if (!appSettingsFolder.exists() && !output.isEmpty()) {
+            Boolean result = appSettingsFolder.mkdir();
+            if (!result) {
+                throw new RuntimeException(Globals.ERROR_FOLDER);
+            }
+        }
 
         for (Apps app : appList) {
             GetAppSettings.doGetAppSettings(app);
