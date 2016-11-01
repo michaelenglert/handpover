f8f1ae2664abcc3dcaefd9194703e24cf491657b
michaelenglert <michi.eng@gmail.com>
2016-11-01 10:01:40 +0100

    Cleanup

--- src/main/java/org/appdynamics/handpover/config/Globals.java
+++ src/main/java/org/appdynamics/handpover/config/Globals.java
@@ -6,6 +6,7 @@ package org.appdynamics.handpover.config;
 @SuppressWarnings("unused")
 public class Globals {
     public static String URL;
+    public static final int PROGRESS = 90;
 
     public static final String CONTROLLER_HTTP = "http://";
     public static final String CONTROLLER_HTTPS = "https://";
@@ -36,7 +37,7 @@ public class Globals {
     public static final String API_APP_EXPORT = "/ConfigObjectImportExportServlet?applicationId=";
     public static final String API_AUDIT = "/ControllerAuditHistory";
     public static final String API_CONTROLLER_CONFIG = "/rest/configuration";
-    public static final String API_DASHBOARD_LIST = "restui/dashboards/list2";
+    public static final String API_DASHBOARD_LIST = "/restui/dashboards/list2";
     public static final String API_GET_USER = "/restui/user/getUser";
     public static final String API_DASHBOARD_EXPORT = "/CustomDashboardImportExportServlet?dashboardId=";
     public static final String API_APP_LIST = "/rest/applications";
@@ -46,15 +47,21 @@ public class Globals {
     public static final String API_BACKEND_LIST = "/backends";
     public static final String API_OUTPUT = "?output=JSON";
     public static final String API_AUDIT_START = "?startTime=";
-    public static final String URL_ENABLE_ACCOUNTS = "?enableAccounts=true";
-    public static final String URL_HOME = "#/location=AD_HOME_OVERVIEW";
     public static final String API_AUDIT_END = "&endTime=";
     public static final String API_CONTROLLER_LOGS = "/FileUploadServlet?action=CONTROLLER_LOGS";
     public static final int API_AUDIT_DAYS = -30;
 
+    public static final String URL_ENABLE_ACCOUNTS = "?enableAccounts=true";
+    public static final String URL_HOME = "#/location=AD_HOME_OVERVIEW";
+
     public static final String EXCEL_FILE = "export.xlsx";
+    public static final String JSON_FILE = ".json";
+    public static final String XML_FILE = ".xml";
     public static final String CONTROLLER_LOG_FILES = "controller-logs.zip";
+    public static final String PHANTOMJS_LOG_FILE = "phantomjsdriver.log";
     public static final String OUTPUT_FOLDER = "output/";
+    public static final String DASHBOARD_FOLDER = OUTPUT_FOLDER + "dashboards/";
+    public static final String APP_SETTINGS_FOLDER = OUTPUT_FOLDER + "app_settings/";
     public static final String OUTPUT_FILE = "output.zip";
     public static final String EXCEL_CONTROLLER_SETTINGS = "controller_settings";
     public static final String EXCEL_CONTROLLER_AUDIT = "controller_audit";
