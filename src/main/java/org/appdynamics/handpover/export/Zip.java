489646922c7bb3dc56506849ae888e1968645720
michaelenglert <michi.eng@gmail.com>
2016-11-01 09:55:19 +0100

    Support to recurse into subfolders

--- src/main/java/org/appdynamics/handpover/export/Zip.java
+++ src/main/java/org/appdynamics/handpover/export/Zip.java
@@ -1,5 +1,6 @@
 package org.appdynamics.handpover.export;
 
+import org.apache.commons.io.FileUtils;
 import org.appdynamics.handpover.config.Globals;
 
 import java.io.File;
@@ -11,44 +12,42 @@ import java.util.zip.ZipOutputStream;
 /**
  * Created by michi on 03.09.16.
  */
+@SuppressWarnings("ResultOfMethodCallIgnored")
 public class Zip {
-    @SuppressWarnings("ResultOfMethodCallIgnored")
-    public static void zipFiles() throws Exception{
-        File sourceFile = new File(Globals.OUTPUT_FOLDER);
-        File[] files = sourceFile.listFiles();
+    public static void zipDirectory() throws Exception {
         FileOutputStream fos = new FileOutputStream(Globals.OUTPUT_FILE);
-
         ZipOutputStream zos = new ZipOutputStream(fos);
+        File sourceDir = new File(Globals.OUTPUT_FOLDER);
+        zipSubDirectory("", sourceDir, zos);
+        zos.close();
+        new File(Globals.PHANTOMJS_LOG_FILE).delete();
+        FileUtils.deleteDirectory(new File(Globals.OUTPUT_FOLDER));
+    }
+
+    private static void zipSubDirectory(String basePath, File dir, ZipOutputStream zos) throws Exception {
+        byte[] buffer = new byte[4096];
+        File[] files = dir.listFiles();
 
         assert files != null;
         for (File file : files) {
-
-            byte[] buffer = new byte[1024];
-
-            FileInputStream fis = new FileInputStream(file);
-
-            zos.putNextEntry(new ZipEntry(file.getName()));
-
-            int length;
-
-            while ((length = fis.read(buffer)) > 0) {
-                zos.write(buffer, 0, length);
+            if (file.isDirectory()) {
+                String path = basePath + file.getName() + "/";
+                zos.putNextEntry(new ZipEntry(path));
+                zipSubDirectory(path, file, zos);
+                zos.closeEntry();
             }
+            else {
+                FileInputStream fis = new FileInputStream(file);
+                zos.putNextEntry(new ZipEntry(basePath + file.getName()));
+                int length;
 
-            zos.closeEntry();
-
-            fis.close();
-
+                while ((length = fis.read(buffer)) > 0) {
+                    zos.write(buffer, 0, length);
+                }
+                zos.closeEntry();
+                fis.close();
+            }
         }
-
-        zos.close();
-
-        for (File file : files){
-            file.delete();
-        }
-
-        new File(Globals.OUTPUT_FOLDER).delete();
-        new File("phantomjsdriver.log").delete();
     }
 
 }
